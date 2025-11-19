1e4c75442d5ad843ece263ed3bac97374210db70
DIOGO PENASSI DE OLIVEIRA <51461855810@imaccommand009.local>
2025-11-19 10:29:14 -0300

    Exemplos js

---
+++ primeira aula.js
@@ -0,0 +1,84 @@
+// window.alert("minha primeira msg")
+
+
+// window.confirm("hoje e sexta?")
+
+//comentario em linha
+
+/**
+ * comentando varias linhas
+ */
+
+
+
+
+// let nome = window.prompt("quel e seu nome?")
+
+// window.alert("bem vindor " + nome)
+
+// console.log("segunda msg")
+
+
+// let numero01 = parseInt(prompt("digite o primeiro numero: "))
+
+// let numero02 = parseInt(prompt("digite o segundo numero: "))
+
+// let soma =numero01 + numero02
+
+// console.log(soma)
+
+
+
+// let num01 = parseFloat(prompt("digite algum numero"))
+
+// let num02 = parseFloat(prompt("digite algum numero"))
+
+// let resultado = num01 = num02
+
+// console.log(resultado)
+
+
+// let idade = parseInt(prompt ("qual e sua idade?"))
+
+// if (idade >= 18) {
+
+//     console.log("voce e maiotr de idade 😁")
+
+// }
+
+// else{
+//     console.log("voce e menor de idade 😔")
+// }
+
+
+let estacoes = prompt("digite uma estaçao do ano: ")
+
+switch (estacoes) {
+    case "verao":
+        console.log("muito calor")
+
+        break;
+
+    case "outono":
+        console.log("as folhas cai no final")
+
+        break;
+
+    case "inverno":
+        console.log("muito frio")
+        break;
+
+    case "primavera":
+        console.log("eu vejo flores em voce")
+        break;
+
+
+
+    default:
+
+        console.log("não existe ewssa estação!!!!")
+        break;
+}
+
+
+
